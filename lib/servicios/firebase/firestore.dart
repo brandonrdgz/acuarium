666d13fa87d3660879e09ee4729efcf1a155e85d
Brandon Rdgz <brandon.rdgz@gmail.com>
2021-11-02 17:44:20 -0600

    Empleo del uid de usuario en lugar de uid aleatorio
    - Para registrar cliente
    - Para registrar negocio

--- lib/servicios/firebase/firestore.dart
+++ lib/servicios/firebase/firestore.dart
@@ -1,36 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Firestore {
-  static Future<DocumentReference<Object?>> registroUsuario({required String correo, required String tipo}) {
+  static Future<void> registroUsuario({required String uid, required String correo, required String tipo}) {
     CollectionReference usuarios = FirebaseFirestore.instance.collection('usuarios');
 
-    return usuarios.add({
+    return usuarios.doc(uid).set({
       'correo': correo,
       'tipo': tipo
     });
   }
 
-  static Future<DocumentReference<Object?>> registroDatosUsuario({
+  static Future<void> registroDatosUsuario({
+    required String uid,
     required String correo,
     required String nombre,
     required String fechaNac,
   }) {
-    CollectionReference usuarios = FirebaseFirestore.instance.collection('datosUsuarios');
+    CollectionReference datosUsuarios = FirebaseFirestore.instance.collection('datosUsuarios');
 
-    return usuarios.add({
+    return datosUsuarios.doc(uid).set({
       'correo': correo,
       'nombre': nombre,
       'fechaNac': fechaNac
     });
   }
 
-  static Future<DocumentReference<Object?>> registroDatosInicialesNegocio({
+  static Future<void> registroDatosInicialesNegocio({
+    required String uid,
     required String correo,
     required String nombreNegocio,
   }) {
     CollectionReference usuarios = FirebaseFirestore.instance.collection('datosNegocios');
 
-    return usuarios.add({
+    return usuarios.doc(uid).set({
       'correo': correo,
       'nombreNegocio': nombreNegocio,
     });
